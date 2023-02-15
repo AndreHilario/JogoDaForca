55aa142eafaf3d05d307e4d940cb0526f7b445fd
Andre Hilario <andre18hilario@gmail.com>
2023-02-15 09:39:54 -0300

    Files changes on public and source to the correct form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Jogo from "./components/Jogo";
 import Letras from "./components/Letras";
+import forca0 from "./assets/forca0.png"
 
 
 export default function App() {
   return (
     <div className="game">
-      <Jogo image="assets/forca0.png" name="Imagem da Forca0"/>
+      <Jogo image={forca0} name="Imagem da Forca0"/>
       <Letras />
     </div>
   )
